71aeca25beece85f64ca1a828ecf9e4f2fdc556a
YoghyPratama <YoghyPratama>
2019-04-16 09:15:18 +0700

    latihan form

---
+++ P4/hasil.php
@@ -0,0 +1,17 @@
+<?php 
+	echo $_POST['nama'];
+	echo '<br>';
+	echo $_POST['email'];
+	echo '<br>';
+	echo $_POST['tempat'];
+	echo '<br>';
+	echo $_POST['semester'];
+	echo '<br>';
+	echo $_POST['jeniskelamin'];
+	echo '<br>';
+	echo $_POST['ckkemampuan'];
+	echo '<br>';
+	echo $_POST['txtalamat'];
+
+
+ ?>
